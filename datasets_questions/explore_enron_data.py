63ae0a20bff411f14faf324a074147e6a0a8103c
adarsh0608 <adarsh108@gmail.com>
2015-07-16 18:01:52 -0700

    questions before documentary

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+# -*- coding: utf-8 -*-
 """ 
     starter code for exploring the Enron dataset (emails + finances) 
     loads up the dataset (pickled dict of dicts)
@@ -16,7 +16,27 @@
 """
 
 import pickle
+import pprint
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+'''
+key in the dictionary is a person’s name and the 
+value is a dictionary containing all the features of that person.
+'''
+count = 0
+for key,value in enron_data.iteritems():
+	if value['poi'] == True:
+		count += 1
+print "Total POI in the list are : ",count
 
+pprint.pprint(enron_data.keys())
+
+#Total stock value of James Prentice
+print enron_data['PRENTICE JAMES']['total_stock_value']
+
+#How many email messages do we have from Wesley Colwell to persons of interest?
+print enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+
+#What’s the value of stock options exercised by Jeffrey Skilling?
+print enron_data['SKILLING JEFFREY K']['exercised_stock_options']
