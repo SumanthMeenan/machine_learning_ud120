988d45653ad98ee4c4e76ffed3f1027d605bc73a
adarsh0608 <adarsh108@gmail.com>
2015-07-21 16:52:47 -0700

    l6 regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -10,12 +10,25 @@
     draws a little scatterplot of the training/testing data
 
     you fill in the regression code where indicated
-
 """    
+'''
+net worth prediction: reg.predict([27])
+slope: reg.coef_
+intercept: reg.intercept_
+r-squared on test dataset: reg.score(ages_test, net_worths_test)
+r-squared on training dataset: reg.score(ages_train, net_worths_train)
+plt.scatter(ages, net_worth)
+plt.plot(ages, reg.predict(ages), color = 'blue', linewidth = 3)
+plt.xlabel('ages')
+plt.ylabel('net worth')
+plt.show()
+'''
 
-
+#Use the salary(input) to predict the bonus(target)
 import sys
 import pickle
+from sklearn import linear_model
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
@@ -23,14 +36,15 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
-train_color = "b"
-test_color = "b"
+train_color = "blue"
+test_color = "red"
 
 
 
@@ -39,10 +53,16 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color from "b" to "r"
 ### to differentiate training points from test points.
 
-
-
-
-
+from sklearn import linear_model
+#create and train the regression
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+#slope
+print "Slope: ", reg.coef_
+#intercept
+print "Intercept: ",reg.intercept_
+print "r-squared on test dataset: ", reg.score(feature_test, target_test)
+print "r-squared on training dataset: " ,reg.score(feature_train, target_train)
 
 
 
